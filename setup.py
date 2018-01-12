b1814ddc49cde9ddb63802fbe73b0448d954d83f
irgendsontyp <htcmaxx@gmail.com>
2018-01-12 19:57:52 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import irgendsontyp
+
+setup(name = "irgendsontyp-helpers",
+      version = irgendsontyp.__version__,
+      description = "Sweet helper utilities of all kinds",
+      author = "irgendsontyp",
+      url = "https://github.com/irgendsontyp/python-irgendsontyp-helpers.git",
+      packages = find_packages()
+     )
